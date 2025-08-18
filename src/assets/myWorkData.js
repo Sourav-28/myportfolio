c005275759b4601ee166e39214975194f8f905db
Sourav-28 <souravjchitnis@gmail.com>
2025-08-19 01:54:49 +0200

    updated_mywork

--- src/assets/myWorkData.js
+++ src/assets/myWorkData.js
@@ -1,10 +1,11 @@
 import graph from './Graph.png'
-
+import netflix from './screenshots/Main.png'
+import news from './screenshots/dark.png'
 const myWorkData = [
     {
         id:1,
         title:"Netflix GPT",
-        w_img:"https://greatstack.in/portfolio/assets/project_1-5xYOfh0z.svg",
+        w_img:netflix,
         github:"https://github.com/Sourav-28/netflix-gpt"
     },
     {
@@ -16,14 +17,14 @@ const myWorkData = [
     {
         id:3,
         title:"Java Banking System",
-        w_img:"https://greatstack.in/portfolio/assets/project_4-KRxErSUy.svg",
+        w_img:"https://miro.medium.com/v2/resize:fit:409/1*x53VGsJAqhN85ReUdQLPzA.jpeg",
         github:"https://github.com/souravjchitnis/JavaBankingSystem"
     },
     {
         id:4,
         title:"News Website",
-        w_img:"https://greatstack.in/portfolio/assets/project_5-u53cBB9v.svg",
-        github:"https://github.com/souravjchitnis/NewsWebsite"
+        w_img:news,
+        github:"https://github.com/Sourav-28/newsapp"
     },
 ]
 
