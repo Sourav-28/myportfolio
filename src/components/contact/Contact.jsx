5f0d8d59dbb8e33870da9b10ea08466e635df50f
Sourav-28 <souravjchitnis@gmail.com>
2025-08-19 02:03:51 +0200

    updated_naming

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import './Contact.css'
+import './contact.css'
 import {theme} from '../../assets/theme.js'
 import {mailIcon,phoneIcon,locationIcon} from '../../assets/icons.js'
 // import { byPrefixAndName } from 'react-fontawesome';
